e698c0c0ec976b7540078372c8999d818927ede9
juanruiz85 <juanruiz85@hotmail.com>
2015-12-30 11:06:08 -0700

    [Update] Mysql server info added

--- database/settings.lua
+++ database/settings.lua
@@ -1,6 +1,6 @@
 mysql = {}
-mysql.username = "mydatabaseusername"
-mysql.password = "mydatabasepassword"
+mysql.username = "root"
+mysql.password = ""
 mysql.port = 3306
 mysql.host = "127.0.0.1"
 mysql.database = "mta"
